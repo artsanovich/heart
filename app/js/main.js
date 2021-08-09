a58ddaec56f59df8d3d8a26d01c273ef29268a7f
artsanovich <artur.kartsan.silva@gmail.com>
2021-08-09 09:06:08 +0300

    before mentor check

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,8 @@ $('.lang-menu__btn').on('click', function(){
   $('.lang-menu__btn').removeClass('lang-menu__btn--active')
   $(this).addClass('lang-menu__btn--active')
 })
+
+$('.projects__link').on('click', function(){
+  $('.projects__link').removeClass('projects__link--active')
+  $(this).addClass('projects__link--active')
+})
